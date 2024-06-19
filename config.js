060fa14d70d3156419fcaa44ccf3d4c52fa16aef
Ken Busse <ken.busse@sofsolv.com>
2024-06-19 14:40:28 +0000

    Pizza

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {}
+
+config.endpoint = 'https://sofsolvmol.documents.azure.com:443/'
+config.key = 'q994dd41DblC2Olwjrd61hk6ng3wIktmndh66oWcHwLZcUrXqK10ZGMNQbzHHI4OhzY2jVSkEKFaACDbl6dZ1w=='
+
+config.database = { id: 'pizzadb' }
+config.container = { id: 'pizzas' }
+
+module.exports = config;
